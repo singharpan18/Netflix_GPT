2d7a6f9bef1b74adab8ee070fc3a780c1fdfc802
arpansingh <arpansingh8583@gmail.com>
2024-08-21 14:43:08 +0530

    responsive

--- zoqqGpt/src/components/Header.js
+++ zoqqGpt/src/components/Header.js
@@ -55,16 +55,18 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black 
+    z-10 flex flex-col md:flex-row justify-between">
       <img
         className='w-44'
+        mx-auto mx-0
         src={LOGO}
         alt="logo"
       />
       {/**display this div when we have user */}
       {
         user && (
-          <div className='flex p-2'>
+          <div className='flex p-2 justify-between'>
             {
               showGptSearch && (
                 <select className='p-2 m-2 bg-gray-900 text-white'
@@ -84,7 +86,7 @@ const Header = () => {
         {showGptSearch ? "Homepage" : "GPT Search"}
       </button>.
 
-          <img className='w-12 h-12'
+          <img className='hidden md:block w-12 h-12'
             alt="usericon"
             src={user?.photoURL}
           />
--- zoqqGpt/src/components/Login.js
+++ zoqqGpt/src/components/Login.js
@@ -75,7 +75,7 @@ const Login = () => {
   return (
     <div>
       <Header />
-      <div className="absolute">
+      <div className="fixed">
         <img src={BG_URL}/>
       </div>
       <form onSubmit={(e) => e.preventDefault()}
